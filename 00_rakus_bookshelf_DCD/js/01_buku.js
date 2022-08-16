bbd9fd898695dbec70440116bd68e763957886f9
fidaatag <70122398+fidaatag@users.noreply.github.com>
2022-08-16 13:00:46 +0700

    add comment

--- 00_rakus_bookshelf_DCD/js/01_buku.js
+++ 00_rakus_bookshelf_DCD/js/01_buku.js
@@ -3,17 +3,20 @@ const unCOMPLETED_BOOK = 'drafSedangDibaca';
 const COMPLETED_BOOK = 'drafSudahTerbaca';
 const BOOK_ITEM = 'itemID';
 
-// mengolah input user menjadi identitas buku
+// ******* mengolah input user menjadi identitas buku *******
 const tambahBuku = () => {
     const uncompletedBOOK = document.getElementById(unCOMPLETED_BOOK);
 
+    // value inputan pertama kali
     const inputJudul = document.getElementById('judul').value;
     const inputPenulis = document.getElementById('penulis').value;
     const inputTahun = document.getElementById('tahun').value;
 
-    const buku = buatBuku(inputJudul, inputPenulis, inputTahun, false)
-    const bukuOBJECT = composeBookOBJECT(inputJudul, inputPenulis, inputTahun, false)
+    // const id = generateId() ----------- tidak dipakai
+    const buku = buatBuku( inputJudul, inputPenulis, inputTahun, false)
+    const bukuOBJECT = composeBookOBJECT( inputJudul, inputPenulis, inputTahun, false)
 
+    // menggabungkan identitas buku dgn id unik
     buku[BOOK_ITEM] = bukuOBJECT.id;
     bukuS.push(bukuOBJECT);
 
@@ -21,7 +24,11 @@ const tambahBuku = () => {
     updateDataBuku();
 }
 
-// membuat identitas buku
+// ******* membuat id unik setiap buku *******
+// function generateId() { return +new Date(); } ----------- tidak dipakai
+
+
+// ******* membuat identitas buku *******
 const composeBookOBJECT = (judul, penulis, tahun, isCompleted) => {
     return {
         id: +new Date(),
@@ -32,6 +39,7 @@ const composeBookOBJECT = (judul, penulis, tahun, isCompleted) => {
     }
 }
 
+// ******* mencari id untuk setiap buku *******
 const cariBuku = (bookID) => {
     for (buku of bukuS) {
         if (buku.id === bookID)
@@ -40,6 +48,7 @@ const cariBuku = (bookID) => {
     return null;
 }
 
+// ******* mencari posisi buku ke-? *******
 const cariBukuINDEX = (bookID) => {
     let index = 0
     for (buku of bukuS) {
